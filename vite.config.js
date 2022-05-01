3ca78fbf4eba97d776a2bd10c173b1242198a49b
Nick <nc.meehan@me.com>
2022-05-01 15:04:24 -0700

    add styling of logo and breakpoints

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import marko from "@marko/vite";
+import ViteFonts from 'vite-plugin-fonts'
+
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
-    plugins: [marko()],
+    plugins: [marko(), ViteFonts({
+      google: {
+        families: ['Krona+One']
+      },
+    })],
     build: {
       outDir: "dist", // Server and client builds should output assets to the same folder.
       emptyOutDir: false // Avoid server / client deleting files from each other.
